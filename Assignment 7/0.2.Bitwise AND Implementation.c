069bdc38e83318a9f5a162e66a0796a25d025525
Tejaswini <tejaswinip972002@gmail.com>
2025-03-23 13:40:25 +0530

    Adding Program

---
+++ Assignment 7/0.2.Bitwise AND Implementation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 3;
+
+    int result = a & b; // Perform bitwise AND
+
+    printf("Bitwise AND of %d and %d is: %d\n", a, b, result);
+
+    return 0;
+}

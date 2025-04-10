651a3986df0bf4d2cab4f175666beafa377fd0c5
Tejaswini <tejaswinip972002@gmail.com>
2025-04-10 12:26:01 +0530

    Adding Program cal sqyare of num

---
+++ 0.1 Basic(Input & Output)/0.17 Accept num from user & cal.square of number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a Number:");
+    scanf("%d",&num);
+
+    printf("square is:%d",num*num);         //square=number*numbar
+
+    return 0;
+}

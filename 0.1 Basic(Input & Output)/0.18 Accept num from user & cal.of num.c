9013bf0aee7533bd4ed81af302f6d1526f4ba329
Tejaswini <tejaswinip972002@gmail.com>
2025-04-11 09:27:47 +0530

    Adding Program

---
+++ 0.1 Basic(Input & Output)/0.18 Accept num from user & cal.of num.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    printf("Cube of number:%d",num*num*num);               //cube=number*number*number
+
+    return 0;
+}

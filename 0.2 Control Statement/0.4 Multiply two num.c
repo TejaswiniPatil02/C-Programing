13974027a2b1fc6415f7f3b5b2b4ab806e5f3dbc
Tejaswini <tejaswinip972002@gmail.com>
2025-04-13 20:40:42 +0530

    Adding Program

---
+++ 0.2 Control Statement/0.4 Multiply two num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2 ,m;
+    printf("Enter num1:");
+    scanf("%d",&num1);
+
+    printf("Enter num2:");
+    scanf("%d",&num2);
+
+    m=num1*num2;
+    printf("The multiplication of %d & %d is: %d",num1,num2,m);
+    return 0;
+}

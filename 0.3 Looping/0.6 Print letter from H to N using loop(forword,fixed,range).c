902a78ca9895c16f571ca8639767278524407c01
Tejaswini <tejaswinip972002@gmail.com>
2025-04-26 15:58:33 +0530

    Adding Program

---
+++ 0.3 Looping/0.6 Print letter from H to N using loop(forword,fixed,range).c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+ {
+    char ch;
+
+    // Loop to print letters from 'H' to 'N'
+    for (ch = 'H'; ch <= 'N'; ch++)
+        {
+        printf("%c ", ch);
+    }
+
+    printf("\n"); // Move to the next line after printing
+
+    return 0;
+}

07541573878af8286b144682c93b848bd490f47a
Tejaswini <tejaswinip972002@gmail.com>
2025-05-21 23:43:14 +0530

    Adding Program

---
+++ 0.6 Array/0.1 Create Array Without Initialization & Display All Elements.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Element at index %d: %d\n", i, arr[i]);
+    }
+
+    return 0;
+}
